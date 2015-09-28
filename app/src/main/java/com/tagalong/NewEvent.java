71418f67c6d57fb249b95502551dbd0346587894
cpf5193 <cpf5193@cs.washington.edu>
2015-09-27 20:53:35 -0700

    Add toolbar to new events page

--- app/src/main/java/com/tagalong/NewEvent.java
+++ app/src/main/java/com/tagalong/NewEvent.java
@@ -4,19 +4,32 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class NewEvent extends Activity implements View.OnClickListener {
+public class NewEvent extends AppCompatActivity implements View.OnClickListener {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_new_event);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    //getSupportActionBar().setDisplayUseLogoEnabled(true);
+    getSupportActionBar().setIcon(R.drawable.tagalong_icon_small);
   }
 
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_new_event, menu);
+    return true;
+  }
 
   @Override
   public void onClick(View v) {
