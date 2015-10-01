973e66f94a9ff0d0e489bc790ea38b4cf3662775
cpf5193 <cpf5193@cs.washington.edu>
2015-09-30 23:13:51 -0700

    Add second db, modularize db switching, add php sanitation

---
+++ app/src/main/java/com/tagalong/Admins.java
@@ -0,0 +1,22 @@
+package com.tagalong;
+
+/**
+ * Created by cpf5193 on 9/30/2015.
+ */
+public class Admins {
+  protected static final String ADMIN = "Chip";
+  // protected static final String ADMIN = "Kyle";
+
+  protected static final String SERVER_ADDRESS = ADMIN.equals("Chip") ?
+    "http://chipfukuhara.com/Tagalong/" :
+    "http://kberg721.freeiz.com/";
+
+  protected static String getRegisterFile() {
+    return SERVER_ADDRESS + (ADMIN.equals("Chip") ? "MySqlRegister.php" : "Register.php");
+  }
+
+  protected String getFetchUserFile() {
+    return SERVER_ADDRESS + (ADMIN.equals("Chip") ? "MySqlFetchUser.php" : 
+      "FetchUserData.php");
+  }
+}
