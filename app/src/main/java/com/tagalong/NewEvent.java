cf864f2e0d1d8e23a37edb9b591ab924deea41a3
kberg721 <kylejbergman@gmail.com>
2015-10-08 18:01:40 -0700

    Basic Autocomplete functionality
    Used google sample code to implement google places autocomplete

--- app/src/main/java/com/tagalong/NewEvent.java
+++ app/src/main/java/com/tagalong/NewEvent.java
@@ -3,15 +3,22 @@ package com.tagalong;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.Spanned;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -21,19 +28,38 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.tagalong.fragments.DatePickerFragment;
 import com.tagalong.fragments.TimePickerFragment;
 
 import java.util.ArrayList;
 
 public class NewEvent extends AppCompatActivity implements View.OnClickListener,
-  DatePickerFragment.OnDateSelectedListener, TimePickerFragment.OnTimeSelectedListener {
+  DatePickerFragment.OnDateSelectedListener, TimePickerFragment.OnTimeSelectedListener,
+  GoogleApiClient.OnConnectionFailedListener {
 
   private ArrayList<Friend> friendsList;
-  Button btnCreateEvent;
+  Button submitNewEvent;
   EditText new_event_name, new_event_location, new_event_invite;
   TagalongDate eventTime;
   DropdownListAdapter dropdownListAdapter;
+  private AutoCompleteTextView mAutocompleteView;
+  private GoogleApiClient mGoogleApiClient;
+  private PlaceAutoCompleteAdapter mAdapter;
+  private TextView mPlaceDetailsText;
+  private TextView mPlaceDetailsAttribution;
+  private static final String TAG = "TagalongPlaces";
+  private static final LatLngBounds BOUNDS_GREATER_SYDNEY = new LatLngBounds(
+    new LatLng(-34.041458, 150.790100), new LatLng(-33.682247, 151.383362));
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -48,9 +74,22 @@ public class NewEvent extends AppCompatActivity implements View.OnClickListener,
     System.out.println("friend list: " + friendsList);
     eventTime = new TagalongDate();
 
-    btnCreateEvent = (Button) findViewById(R.id.submitNewEvent);
+    mAutocompleteView = (AutoCompleteTextView) findViewById(R.id.autocomplete_places);
+    mGoogleApiClient = new GoogleApiClient
+      .Builder(this)
+      .addApi(Places.GEO_DATA_API)
+      .addOnConnectionFailedListener(this)
+      .build();
+
+    mPlaceDetailsText = (TextView) findViewById(R.id.place_details);
+    mPlaceDetailsAttribution = (TextView) findViewById(R.id.place_attribution);
+
+    mAdapter = new PlaceAutoCompleteAdapter(this, mGoogleApiClient, BOUNDS_GREATER_SYDNEY,
+      null);
+    mAutocompleteView.setAdapter(mAdapter);
+
+    submitNewEvent = (Button) findViewById(R.id.submitNewEvent);
     new_event_name = (EditText) findViewById(R.id.new_event_name);
-    new_event_location = (EditText) findViewById(R.id.new_event_location);
 
     //onClickListener to initiate the dropDown list
     Button inviteButton = (Button)findViewById(R.id.inviteButton);
@@ -59,6 +98,21 @@ public class NewEvent extends AppCompatActivity implements View.OnClickListener,
         initiatePopUp(friendsList);
       }
     });
+
+    mAutocompleteView.setOnItemClickListener(mAutocompleteClickListener);
+    submitNewEvent.setOnClickListener(this);
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    mGoogleApiClient.connect();
+  }
+
+  @Override
+  protected void onStop() {
+    mGoogleApiClient.disconnect();
+    super.onStop();
   }
 
   private void initiatePopUp(ArrayList<Friend> friendsList) {
@@ -79,7 +133,7 @@ public class NewEvent extends AppCompatActivity implements View.OnClickListener,
     //the pop-up will be dismissed if touch event occurs anywhere outside its window
     pw.setTouchInterceptor(new View.OnTouchListener() {
       public boolean onTouch(View v, MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_UP) {
+        if (event.getAction() == MotionEvent.ACTION_OUTSIDE) {
           pw.dismiss();
           return true;
         }
@@ -120,13 +174,14 @@ public class NewEvent extends AppCompatActivity implements View.OnClickListener,
         int messageResId = 0; //will be used to make toast
         ArrayList<Friend> invitedFriends = dropdownListAdapter.getSelectedFriends();
         String eventName = new_event_name.getText().toString();
-        String eventLocation = new_event_location.getText().toString();
         String eventGuestList = new_event_invite.getText().toString();
-        if(eventGuestList == "" || eventName == "" || eventLocation == "" || !isTimeSet()) {
+        if(invitedFriends.size() == 0 || eventName == "" || !isTimeSet()) {
           messageResId = R.string.missing_event_field;
         }
         if(messageResId == 0) {
-          Event event = new Event(eventName, eventLocation, eventTime.toString(), eventGuestList);
+          /*TODO: Parse invitedFriends into eventGuestList
+           */
+          Event event = new Event(eventName, "", eventTime.toString(), eventGuestList);
           submitEvent(event);
         } else {
           Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
@@ -172,5 +227,87 @@ public class NewEvent extends AppCompatActivity implements View.OnClickListener,
   public boolean isTimeSet() {
     return !(eventTime.getmDay() == 0 || eventTime.getmMonth() == 0 || eventTime.getmYear() == 0 ||
       eventTime.getmHour() == 0 || eventTime.getmMinute() == 0);
+  }
+
+  private AdapterView.OnItemClickListener mAutocompleteClickListener
+    = new AdapterView.OnItemClickListener() {
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            /*
+             Retrieve the place ID of the selected item from the Adapter.
+             The adapter stores each Place suggestion in a AutocompletePrediction from which we
+             read the place ID and title.
+              */
+      final AutocompletePrediction item = mAdapter.getItem(position);
+      final String placeId = item.getPlaceId();
+      final CharSequence primaryText = item.getPrimaryText(null);
+
+      Log.i(TAG, "Autocomplete item selected: " + primaryText);
+
+            /*
+             Issue a request to the Places Geo Data API to retrieve a Place object with additional
+             details about the place.
+              */
+      PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
+        .getPlaceById(mGoogleApiClient, placeId);
+      placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+
+      Toast.makeText(getApplicationContext(), "Clicked: " + primaryText,
+        Toast.LENGTH_SHORT).show();
+      Log.i(TAG, "Called getPlaceById to get Place details for " + placeId);
+    }
+  };
+
+  private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback
+    = new ResultCallback<PlaceBuffer>() {
+    @Override
+    public void onResult(PlaceBuffer places) {
+      if (!places.getStatus().isSuccess()) {
+        // Request did not complete successfully
+        Log.e(TAG, "Place query did not complete. Error: " + places.getStatus().toString());
+        places.release();
+        return;
+      }
+      // Get the Place object from the buffer.
+      final Place place = places.get(0);
+
+      // Format details of the place for display and show it in a TextView.
+      mPlaceDetailsText.setText(formatPlaceDetails(getResources(), place.getName(),
+        place.getId(), place.getAddress(), place.getPhoneNumber(),
+        place.getWebsiteUri()));
+
+      // Display the third party attributions if set.
+      final CharSequence thirdPartyAttribution = places.getAttributions();
+      if (thirdPartyAttribution == null) {
+        mPlaceDetailsAttribution.setVisibility(View.GONE);
+      } else {
+        mPlaceDetailsAttribution.setVisibility(View.VISIBLE);
+        mPlaceDetailsAttribution.setText(Html.fromHtml(thirdPartyAttribution.toString()));
+      }
+
+      Log.i(TAG, "Place details received: " + place.getName());
+
+      places.release();
+    }
+  };
+
+  private static Spanned formatPlaceDetails(Resources res, CharSequence name, String id,
+                                            CharSequence address, CharSequence phoneNumber, Uri websiteUri) {
+    Log.e(TAG, res.getString(R.string.place_details, name, id, address, phoneNumber,
+      websiteUri));
+    return Html.fromHtml(res.getString(R.string.place_details, name, id, address, phoneNumber,
+      websiteUri));
+
+  }
+
+  @Override
+  public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    Log.e(TAG, "onConnectionFailed: ConnectionResult.getErrorCode() = "
+      + connectionResult.getErrorCode());
+
+    Toast.makeText(this,
+      "Could not connect to Google API Client: Error " + connectionResult.getErrorCode(),
+      Toast.LENGTH_SHORT).show();
   }
 }
